43eaa64e4a09a791e152bfe3be81b3379cb6b5f6
Дмитро Затилюк <ipz223_zdo@student.ztu.edu.ua>
2024-04-26 18:21:40 +0300

    Change List

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const hbs = require("hbs");
 const axios = require('axios');
+const fs = require('fs');
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 let app = express();
 
 app.set('view engine', 'hbs');
 
+const cities = JSON.parse(fs.readFileSync('cities.json', 'utf-8'));
+
 app.get('/', (req, res) => {
     res.render('main.hbs');
 });
 
 app.get('/weather/:city', async (req, res) => {
-    const city = req.params.city;
+    let city = req.params.city;
 
     if (!city) {
         return res.send('Please provide a city name');
@@ -24,8 +27,7 @@ app.get('/weather/:city', async (req, res) => {
 
         const response = await axios.get(apiUrl);
         const weatherData = response.data;
-
-        res.render('weather.hbs', { city, weather: weatherData });
+        res.render('weather.hbs', { city, cities, weather: weatherData  });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
@@ -35,6 +37,7 @@ app.get('/weather/:city', async (req, res) => {
 app.listen(3000, "127.0.0.1", () => {
     console.log("Example app listening on port 3000");
 });
+
 
 
 //nodemon -e js,hbs,json app
